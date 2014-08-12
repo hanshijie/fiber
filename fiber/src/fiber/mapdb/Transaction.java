4ef8d4bc29e00de2bdf33aee64e9ac8d4e82f02a
HuangQiang <taojingjian@gmail.com>
2014-08-12 19:10:43 +0800

    1. 重构 TxnLogger. 改成 Transaction 内部类Logger.

--- fiber/src/fiber/mapdb/Transaction.java
+++ fiber/src/fiber/mapdb/Transaction.java
@@ -1,22 +1,99 @@
 package fiber.mapdb;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeSet;
+
 import fiber.common.LockPool;
+import fiber.io.Const;
 import fiber.io.Log;
+import static fiber.io.Log.*;
 
 public class Transaction {
+	
+	final static class Logger {
+		private static final class LogInfo {
+			public final String level;
+			public final String msg;
+			LogInfo(String l, String f) {
+				this.level = l;
+				this.msg = f;
+			}
+		}
+		private ArrayList<LogInfo> logs = new ArrayList<LogInfo>();		
+		public void debug(String fmt, Object... objects) {
+			if(Const.log_level <= LOG_DEBUG) {
+				logs.add(new LogInfo("debug", String.format(fmt, objects)));
+			}
+		}
+		
+		public void info(String fmt, Object... objects) {
+			if(Const.log_level <= LOG_INFO) {
+				logs.add(new LogInfo("info", String.format(fmt, objects)));
+			}
+		}
+		
+		
+		public void trace(String fmt, Object... objects) {
+			if(Const.log_level <= LOG_TRACE) {
+				logs.add(new LogInfo("trace", String.format(fmt, objects)));
+			}
+		}
+		
+		public void notice(String fmt, Object... objects) {
+			if(Const.log_level <= LOG_NOTICE) {
+				logs.add(new LogInfo("notice", String.format(fmt, objects)));
+			}
+		}
+		
+		public void warn(String fmt, Object... objects) {
+			if(Const.log_level <= LOG_WARN) {
+				logs.add(new LogInfo("warn", String.format(fmt, objects)));
+			}
+		}
+		
+		public void err(String fmt, Object... objects) {
+			if(Const.log_level <= LOG_ERR) {
+				logs.add(new LogInfo("err", String.format(fmt, objects)));
+			}
+		}
+		
+		public void alert(String fmt, Object... objects) {
+			if(Const.log_level <= LOG_ALERT) {
+				logs.add(new LogInfo("alert", String.format(fmt, objects)));
+			}
+		}
+		
+		public void fatal(String fmt, Object... objects) {
+			if(Const.log_level <= LOG_FATAL) {
+				logs.add(new LogInfo("fatal", String.format(fmt, objects)));
+			}
+		}
+		
+		public void clear() {
+			logs.clear();
+		}
+
+		public void commit() {
+			for(LogInfo log : this.logs) {
+				Log.logSimple(log.level, log.msg);
+			}
+		}
+
+	}
+
+	
 	private final HashMap<WKey, WValue> dataMap;
-	private final TxnLogger logger;	
+	private final Logger logger;	
 	private final TreeSet<Integer> lockSet;
 	public Transaction() {
 		this.dataMap = new HashMap<WKey, WValue>();
-		this.logger = new TxnLogger();
+		this.logger = new Logger();
 		this.lockSet = new TreeSet<Integer>();
 	}
 	
-	public final TxnLogger getLogger() {
+	public final Logger getLogger() {
 		return this.logger;
 	}
 
